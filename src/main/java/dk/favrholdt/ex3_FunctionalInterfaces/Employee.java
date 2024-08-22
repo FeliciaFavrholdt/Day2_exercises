e240001a75495b3ed1fec9b31705d3c9ca95d0a1
Felicia Favrholdt <fillefilejs@Felicias-MacBook-Air.local>
2024-08-22 14:32:50 +0200

    updates files for exercise 3

---
+++ src/main/java/dk/favrholdt/ex3_FunctionalInterfaces/Employee.java
@@ -0,0 +1,45 @@
+package dk.favrholdt.ex3_FunctionalInterfaces;
+
+public class Employee {
+    //attributes
+    private String name;
+    private int age;
+
+    //constructor
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    //no-args constructor
+    public Employee() {
+    }
+
+    //getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    //toString method
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/main/java/dk/favrholdt/ex3_FunctionalInterfaces/Examples.java
+++ src/main/java/dk/favrholdt/ex3_FunctionalInterfaces/Examples.java
@@ -1,8 +1,11 @@
 package dk.favrholdt.ex3_FunctionalInterfaces;
 
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
@@ -30,25 +33,51 @@ public class Examples {
 
         //3.2 Use predicate to filter a list of integers, so only those divisible by 7 remain
         List<Integer> numbers = Arrays.asList(14, 9661, 5, 40, 3544, 49, 500, 700);
-
         Predicate<Integer> isDivisibleBySeven = num -> num % 7 == 0;
 
         System.out.println("Numbers divisible by 7:" + numbers.stream()
                 .filter(isDivisibleBySeven)
                 .toList());
 
+        //3.3 Use Supplier to create a list of Employee objects based on a list of names like Arrays.asList("John", "Jane", "Jack", "Joe", "Jill")
+        Supplier<List<Employee>> createEmployees = getListSupplier();
+
+        //3.4 Use Consumer to print the list of Employee objects
+        Consumer<Employee> printEmployee = System.out::println;
+        printEmployee.accept(createEmployees.get().get(0)); // Output: Employee{name='John', age=45}
+
+        //3.5 Use Function to convert a list of Employee objects to a list of names
+        Function<List<Employee>, List<String>> convertToNames = convertObjectsToListOfNames();
+        System.out.println("Names: " + convertToNames.apply(createEmployees.get()));
+
+        //3.6 Use Predicate to check if a given employee is older than 18
+        Predicate<Employee> isOlderThan = (employee) -> employee.getAge() > 18;
+        System.out.println("Is the employee older than 18? " + isOlderThan.test(createEmployees.get().get(0))); // Output: true
+
+
     }
 
-    //3.3 Use Supplier to create a list of Employee objects based on a list of names like Arrays.asList("John", "Jane", "Jack", "Joe", "Jill").
+    private static Supplier<List<Employee>> getListSupplier() {
+        List<String> names = Arrays.asList("John", "Jane", "Jack", "Joe", "Jill");
 
-    //3.4 Use Consumer to print the list of Employee objects
+        Supplier<List<Employee>> createEmployees = () -> {
+            List<Employee> employees = new ArrayList<>();
+            for (String name : names) {
+                employees.add(new Employee(name, new Random().nextInt(80)));
+            }
+            return employees;
+        };
+        return createEmployees;
+    }
 
-    //3.5 Use Function to convert a list of Employee objects to a list of names.
+    private static Function<List<Employee>, List<String>> convertObjectsToListOfNames() {
+        List<Employee> employees = getListSupplier().get();
+        Function<Employee, String> getNameFunction = Employee::getName;
 
-    //3.6 Use Predicateto check if a given employee is older than 18.
+        List<String> names = employees.stream()
+                .map(getNameFunction)
+                .toList();
 
-
-
-
-
+        return (list) -> names;
+    }
 }
